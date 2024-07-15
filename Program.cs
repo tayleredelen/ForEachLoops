84515346a05c3da41d48f2f91caa3ee78ee2eadb
tayleredelen <taylerledelen@gmail.com>
2024-07-15 13:12:12 -0500

    chapter complete

---
+++ Program.cs
@@ -0,0 +1,31 @@
+using System;
+
+namespace Iterations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // var name = "Tae Albert";
+
+            // for (var i = 0; i < name.Length; i++)
+            // {
+            //     Console.WriteLine(name[i]);
+            // }
+
+            // foreach (var character in name)
+            // {
+            //     Console.WriteLine(character);
+            // }
+
+            var numbers = new int[] {1, 2, 3, 4};
+
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+
+
+        }
+    }
+}
